8e9de316bbc8da688ff83966b8755990ad08110f
Michael Blix <mkeblx@gmail.com>
2016-05-24 22:41:55 -0400

    Add merged geometry method

--- js/CubeSea.js
+++ js/CubeSea.js
@@ -31,3 +31,38 @@ function CubeSea( gridSize, cubeSize, lit ) {
 
 	return sea;
 }
+
+
+// Singule mesh
+function CubeSea2( gridSize, cubeSize, lit ) {
+	var sea = new THREE.Object3D();
+
+	new THREE.TextureLoader().load( 'textures/cube-sea.png', function( map ){
+
+	var _geo = new THREE.Geometry();
+	var geo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
+
+	for ( var x = 0; x < gridSize; ++x ) {
+		for ( var y = 0; y < gridSize; ++y ) {
+			for ( var z = 0; z < gridSize; ++z ) {
+				var position = new THREE.Vector3( x - gridSize/2, y - gridSize/2, z - gridSize/2 );
+				if ( position.x == 0 && position.y == 0 && position.z == 0 )
+					continue;
+
+				_geo.merge( geo, new THREE.Matrix4().makeTranslation( position.x, position.y, position.z ) );
+			}
+		}
+	}
+
+	var mat = new (!lit ? THREE.MeshBasicMaterial : THREE.MeshLambertMaterial)({
+		color: 0xffffff,
+		map: map
+	});
+	var _cubes = new THREE.Mesh( _geo, mat );
+
+	sea.add( _cubes );
+
+	});
+
+	return sea;
+}
