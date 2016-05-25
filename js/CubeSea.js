637d318f0d58d652745bfa0c7137b6406115ae99
Michael Blix <mkeblx@gmail.com>
2016-05-24 20:51:25 -0400

    Add lighting to scene

--- js/CubeSea.js
+++ js/CubeSea.js
@@ -2,13 +2,13 @@
 CubeSea
 */
 
-function CubeSea( gridSize, cubeSize ) {
+function CubeSea( gridSize, cubeSize, lit ) {
 	var sea = new THREE.Object3D();
 
 	new THREE.TextureLoader().load( 'textures/cube-sea.png', function( map ){
 
 	var geo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
-	var mat = new THREE.MeshBasicMaterial({
+	var mat = new (!lit ? THREE.MeshBasicMaterial : THREE.MeshLambertMaterial)({
 		color: 0xffffff,
 		map: map
 	});
@@ -17,10 +17,11 @@ function CubeSea( gridSize, cubeSize ) {
 	for ( var x = 0; x < gridSize; ++x ) {
 		for ( var y = 0; y < gridSize; ++y ) {
 			for ( var z = 0; z < gridSize; ++z ) {
-				if ( x == 0 && y == 0 && z == 0 )
-					continue;
 				var cube = _cube.clone();
-				cube.position.set( x - gridSize/2, y - gridSize/2, z - gridSize/2 );
+				var position = new THREE.Vector3( x - gridSize/2, y - gridSize/2, z - gridSize/2 );
+				if ( position.x == 0 && position.y == 0 && position.z == 0 )
+					continue;
+				cube.position.copy( position );
 				sea.add( cube );
 			}
 		}
