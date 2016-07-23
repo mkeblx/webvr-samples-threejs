868da5e1163cc8204fc0858f67583a8053b520d5
Michael Blix <mkeblx@gmail.com>
2016-07-22 20:01:46 -0400

    unify merge into function

--- js/CubeSea.js
+++ js/CubeSea.js
@@ -2,45 +2,20 @@
 CubeSea
 */
 
-function CubeSea( gridSize, cubeSize, lit ) {
-	var sea = new THREE.Object3D();
-
-	new THREE.TextureLoader().load( 'textures/cube-sea.png', function( map ){
-
-	var geo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
-	var mat = new (!lit ? THREE.MeshBasicMaterial : THREE.MeshLambertMaterial)({
-		color: 0xffffff,
-		map: map
-	});
-	var _cube = new THREE.Mesh( geo, mat );
-
-	for ( var x = 0; x < gridSize; ++x ) {
-		for ( var y = 0; y < gridSize; ++y ) {
-			for ( var z = 0; z < gridSize; ++z ) {
-				var cube = _cube.clone();
-				var position = new THREE.Vector3( x - gridSize/2, y - gridSize/2, z - gridSize/2 );
-				if ( position.x == 0 && position.y == 0 && position.z == 0 )
-					continue;
-				cube.position.copy( position );
-				sea.add( cube );
-			}
-		}
-	}
-
-	});
-
-	return sea;
-}
-
-
-// Singule mesh
-function CubeSea2( gridSize, cubeSize, lit ) {
+// lit : lambert or flat material
+// merge : merge geometry into single mesh
+function CubeSea( gridSize, cubeSize, lit, merge ) {
 	var sea = new THREE.Object3D();
 
 	new THREE.TextureLoader().load( 'textures/cube-sea.png', function( map ){
 
 	var _geo = new THREE.Geometry();
 	var geo = new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize );
+	var mat = new (lit ? THREE.MeshLambertMaterial : THREE.MeshBasicMaterial)({
+		map: map
+	});
+	if (!merge)
+		var _cube = new THREE.Mesh( geo, mat );
 
 	for ( var x = 0; x < gridSize; ++x ) {
 		for ( var y = 0; y < gridSize; ++y ) {
@@ -49,18 +24,21 @@ function CubeSea2( gridSize, cubeSize, lit ) {
 				if ( position.x == 0 && position.y == 0 && position.z == 0 )
 					continue;
 
-				_geo.merge( geo, new THREE.Matrix4().makeTranslation( position.x, position.y, position.z ) );
+				if (!merge) {
+					var cube = _cube.clone();
+					cube.position.copy( position );
+					sea.add( cube );
+				} else {
+					_geo.merge( geo, new THREE.Matrix4().makeTranslation( position.x, position.y, position.z ) );
+				}
 			}
 		}
 	}
 
-	var mat = new (!lit ? THREE.MeshBasicMaterial : THREE.MeshLambertMaterial)({
-		color: 0xffffff,
-		map: map
-	});
-	var _cubes = new THREE.Mesh( _geo, mat );
-
-	sea.add( _cubes );
+	if (merge) {
+		var _cubes = new THREE.Mesh( _geo, mat );
+		sea.add( _cubes );
+	}
 
 	});
 
